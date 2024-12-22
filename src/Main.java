64fdcf12381ed680692475465182180216600d40
Konrad M <konrad.michalak2003work@gmail.com>
2024-12-23 00:38:03 +0100

    Zadanie 3 wykonane

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String inputFilePath = "C:\\Users\\kmichala\\IdeaProjects\\PJ_Lab10\\src\\plik.txt";
+        String outputFilePath = "C:\\Users\\kmichala\\IdeaProjects\\PJ_Lab10\\src\\plik_kopia.txt";
+
+        // Try-with-resources dla automatycznego zarządzania zasobami
+        try (FileReader reader = new FileReader(inputFilePath);
+             FileWriter writer = new FileWriter(outputFilePath)) {
+
+            int character;
+            while ((character = reader.read()) != -1) {
+                // Zamiana spacji na myślnik
+                if (character == ' ') {
+                    writer.write('-');
+                } else {
+                    writer.write(character);
+                }
+            }
+            System.out.println("Plik został skopiowany z zamianą spacji na myślniki.");
+
+        } catch (IOException e) {
+            System.out.println("Wystąpił błąd podczas kopiowania pliku: " + e.getMessage());
+        }
     }
 }
